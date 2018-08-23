6cfd80efe6894882f41b7ce4ef7f1383848a9f70
Anthony la'O <anthonylao@live.com>
2018-08-23 09:18:22 +0000

    Cleaned up code and conditions

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -13,9 +13,10 @@ def all_place(city_id):
     """ Retrieves the list of all Place objects of a City """
     places_obj = []
     city_obj = storage.get('City', city_id)
-    if city_id is None:
+    if city_obj is None:
         abort(404)
     for key, value in storage.all("Place").items():
+        if value.city_id == city_id:
             places_obj.append(value.to_dict())
     return jsonify(places_obj)
 
@@ -48,22 +49,21 @@ def create_place(city_id):
     """ Creates a Place """
     req = request.get_json()
     city_obj = storage.get("City", city_id)
-    user_obj = storage.get("User", data['user_id'])
-    if req is None:
-        return (jsonify({'error': 'Not a JSON'}), 400)
+    user_obj = storage.get("User", req['user_id'])
     if city_obj is None:
         abort(404)
+    if not req:
+        return (jsonify({'error': 'Not a JSON'}), 400)
     if 'user_id' not in req:
         return (jsonify({'error': 'Missing user_id'}), 400)
+    if user_obj is None:
+        abort(404)
     if 'name' not in req:
         return (jsonify({'error': 'Missing name'}), 400)
-    if user_obj:
-        req['city_id'] == city_id
-        new_place = Place(**req)
-        new_place.save()
-        return (jsonify(new_place.to_dict()), 201)
-    else:
-        abort(404)
+    req['city_id'] == city_id
+    new_place = Place(**req)
+    new_place.save()
+    return (jsonify(new_place.to_dict()), 201)
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
@@ -71,14 +71,12 @@ def update_place(place_id):
     """ Updates a Place object """
     req = request.get_json()
     place_obj = storage.get("Place", place_id)
-    if req is None:
-        return (jsonify({'error': "Not a JSON"}), 400)
     if place_obj is None:
         abort(404)
-    else:
-        for key, val in req.items():
-            if key not in 'id' and key not in 'created_at' and\
-               key not in 'user_id' and key not in 'city_id':
-                setattr(place_obj, key, val)
+    if not req:
+        return (jsonify({'error': "Not a JSON"}), 400)
+    for key, val in req.items():
+        if key not in ['id', 'created_at', 'user_id', 'city_id']:
+            setattr(place_obj, key, val)
     place_obj.save()
     return (jsonify(my_place.to_dict()), 200)
