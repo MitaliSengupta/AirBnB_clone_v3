43749bfbd56cbdbff6640e8be7ed5e4702fd4aee
Anthony la'O <anthonylao@live.com>
2018-08-23 11:55:52 +0000

    Variable fixes and checker tests

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -3,7 +3,7 @@
 Review Module
 """
 from api.v1.views import app_views
-from flask import jsonify, abort, request, Blueprint
+from flask import jsonify, abort, request
 from models import storage, Review, Place
 
 
@@ -15,8 +15,8 @@ def all_reviews(place_id):
     if place_obj is None:
         abort(404)
     rev_obj = []
-    rev_str = storage.all("Review").items()
-    for key, value in rev_str:
+    rev_str = storage.all("Review")
+    for key, value in rev_str.items():
         if value.place_id == place_id:
             rev_obj.append(value.to_dict())
     return jsonify(rev_obj)
@@ -25,11 +25,11 @@ def all_reviews(place_id):
 @app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
 def indv_review(review_id):
     """ Retrieves a Review object """
-    try:
-        rev_obj = storage.get("Review", review_id)
-        return jsonify(rev_obj.to_dict())
-    except Exception:
+    rev_obj = storage.get("Review", review_id)
+    if rev_obj is None:
         abort(404)
+    else:
+        return jsonify(rev_obj.to_dict())
 
 
 @app_views.route('/reviews/<review_id>', methods=['DELETE'],
@@ -63,28 +63,24 @@ def create_review(place_id):
     if "text" not in rev_req:
         return jsonify({"error": "Missing text"}), 400
     else:
-        ruid = rev_req["user_id"]
-        rxt = rev_req["text"]
-        rev = Review(user_id=ruid, text=rxt, place_id=place_id)
-        for key, value in rev_req.items():
-            setattr(rev, key, value)
-        rev.save()
-        return jsonify(rev.to_dict()), 201
+        rev_obj = Review(**rev_req)
+        rev_obj.place_id = place_id
+        rev_obj.save()
+        return jsonify(rev_obj.to_dict()), 201
 
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def update_review(review_id):
     """ Updates a Review object """
     rev_obj = storage.get('Review', review_id)
-    if not rev_obj:
+    if rev_obj is None:
         abort(404)
-    req = request.json()
+    req = request.get_json()
     if not req:
         return jsonify({"error": "Not a JSON"}), 400
-    rev_json = request.get_json()
     bypass = ["id", "place_id", "user_id", "created_at", "updated_at"]
-    for key, val in rev_json.items():
+    for key, val in req.items():
         if key not in bypass:
-            setattr(rev_obj, key, val)
+            setattr(rev_obj, key, value)
     rev_obj.save()
     return (jsonify(rev_obj.to_dict()), 200)
