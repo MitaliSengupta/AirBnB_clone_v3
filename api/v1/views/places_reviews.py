8aa6438360ffe8b352c4d83301ff9e43f32f1232
Anthony la'O <anthonylao@live.com>
2018-08-23 11:24:32 +0000

    Checked for edge cases

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -48,24 +48,25 @@ def del_review(review_id):
                  strict_slashes=False)
 def create_review(place_id):
     """ Creates a Review """
-    req = request.get_json()
-    place_obj = storage.get('Place', place_id)
-    user_obj = storage.get('User', req['user_id'])
+    place_obj = storage.get("Place", place_id)
     if place_obj is None:
         abort(404)
-    if req is None:
+    req = request.json
+    if not req:
         return jsonify({"error": "Not a JSON"}), 400
-    if 'user_id' not in req:
+    rev_req = request.get_json()
+    if "user_id" not in rev_req:
         return jsonify({"error": "Missing user_id"}), 400
+    user_obj = storage.get("User", rev_req["user_id"])
     if user_obj is None:
         abort(404)
-    if 'text' not in req:
+    if "text" not in rev_req:
         return jsonify({"error": "Missing text"}), 400
     else:
-        ruid = req["user_id"]
-        rxt = req["text"]
+        ruid = rev_req["user_id"]
+        rxt = rev_req["text"]
         rev = Review(user_id=ruid, text=rxt, place_id=place_id)
-        for key, value in req.items():
+        for key, value in rev_req.items():
             setattr(rev, key, value)
         rev.save()
         return jsonify(rev.to_dict()), 201
@@ -75,18 +76,15 @@ def create_review(place_id):
 def update_review(review_id):
     """ Updates a Review object """
     rev_obj = storage.get('Review', review_id)
-    req = request.get_json()
     if not rev_obj:
         abort(404)
+    req = request.json()
     if not req:
         return jsonify({"error": "Not a JSON"}), 400
-    for key, val in req.items():
-        if key not in [
-            'id',
-            'created_at',
-            'user_id',
-            'place_id',
-                'updated_at']:
+    rev_json = request.get_json()
+    for key, val in rev_json.items():
+        if key not in ['id', 'created_at', 'user_id',\
+                       'place_id', 'updated_at']:
             setattr(rev_obj, key, val)
     rev_obj.save()
     return (jsonify(rev_obj.to_dict()), 200)
